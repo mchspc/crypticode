42c803cb9daa21b974b2e80d6b90d8aa309120f5
Anish Ahuja <ahujaanishll@gmail.com>
2021-03-23 18:57:02 -0700

    Updated data.py skeleton

--- data.py
+++ data.py
@@ -7,25 +7,27 @@ https://github.com/mchspc/crypticode/blob/main/data-cheat.md
 
 Goals
 -----
-- Open data file at startup
-- Close data file when done
+- Open data file at startup and close to ensure file exists/is created
 - Read data from file
 - Turn that data (titles & notes) into lists/dict
 - Write new data to the file
+- Close data file at the end of all functions
 
 Good luck!
 '''
 
 def startup():
-    # open the file & return the variable
+    # open the file so that if it does not already exist, it will be created (use appropriate file permissions from cheat sheet to help)
+    # close file right after
 
-def read_file(recfile):
+def read_file():
+    # open file with appropriate permissions
     # read from file
-    # separate the titles & content
+    # separate the titles & content so it can be returned to main.py
+    # close file after
 
-def write_file(recfile, new_title, new_content):
+def write_file(new_title, new_content):
+    # open file with appropriate permissions
     # write new data (from args)
-    # organize it so it can be re-read
-
-def shutdown(recfile):
-    # close the file when user exits main.py
+    # organize it so it can be re-read (consistent with methods in read_file())
+    # close file after
